26b8790d260cc380739272e5e5689e3f55714456
Corey Mitchell <cmitche09@gmail.com>
2021-12-06 02:53:26 -0500

    Clean up the code and add files to .gitignore

--- batteryApp.js
+++ batteryApp.js
@@ -1,11 +1,10 @@
 
 const fs = require('fs');
-const { exec, execFile } = require('child_process');
 
 // Allow users to set their own battery notification thresholds
 //  - config file
 let highThreshold = 80; // 80% charge
-let lowThreshold = 37; // 35% charge
+let lowThreshold = 35; // 35% charge
 
 let chargeComputer = false;
 
@@ -30,8 +29,6 @@ fs.promises.readFile('./sampleOutput.json', 'utf-8')
                     console.log(err);
                 }
             });
-            // console.log('SUFFICIENTLY CHARGED: shortcuts run Unplug Computer');
-            
         } else if(battery.percentage <= lowThreshold && battery.isCharging === "FALSE"){
             chargeComputer = 1;
             fs.writeFile('./result.txt', `${chargeComputer}`, (err) => {
@@ -39,39 +36,7 @@ fs.promises.readFile('./sampleOutput.json', 'utf-8')
                     console.log(err);
                 }
             });
-            // console.log('BATTERY LOW: shortcuts run Charge Computer');
         };
-        console.log(chargeComputer);
         return chargeComputer;
-    })
-    .then( indicator => {
-        // console.log(indicator);
-        if (indicator === 0){
-            // exec('bash unplugCharger.sh',{shell: '/bin/bash'}, (err, stdout, stderr) => {
-            //     if (err){
-            //         console.error(err);
-            //         return;
-            //     }
-            //     if (stderr){
-            //         console.log(stderr);
-            //         return;
-            //     }
-            //     console.log("message");
-            //     console.log(stdout);
-            // });
-        }
-        if (indicator === 1){
-            // exec('bash chargeComputer.sh',{shell: '/bin/bash'}, (err, stdout, stderr) => {
-            //     if (err){
-            //         console.error(err);
-            //         return;
-            //     }
-            //     if (stderr){
-            //         console.log(stderr);
-            //         return;
-            //     }
-            //     console.log(stdout);
-            // });
-        }
     })
     .catch(err => console.error(err));
